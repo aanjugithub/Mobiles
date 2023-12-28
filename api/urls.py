126ad1328fc2172620b5270598a6c27c84116972
Anju <anjubijumon133@gmail.com>
2023-12-28 09:51:39 +0530

    viewset commit

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from api import views
+from rest_framework.routers import DefaultRouter
 
-
+router=DefaultRouter()
+router.register("v1/mobiles",views.MobileViewSetView,basename="mobiles")
 
 
 urlpatterns=[
     path("mobiles/",views.MobileListCreateView.as_view()),
     path("mobiles/<int:pk>/",views.MobileDetailUpdateDestroyView.as_view())
-]
+] + router.urls
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from mobile.models import Mobiles
 from api.serializer import MobileSerializer
@@ -47,4 +48,39 @@ class MobileDetailUpdateDestroyView(APIView):
         id=kwargs.get("pk")
         Mobiles.objects.get(id=id).delete()
         return Response(data={"message":"data deleted"})
+    
+class MobileViewSetView(viewsets.ViewSet):
+    def list(self,request,*args,**kwargs):
+        qs=Mobiles.objects.all()
+        serializer=MobileSerializer(qs,many=True) #deser
+        return Response(data=serializer.data)
+    
+    def retrieve(self,request,*args,**kwargs):
+        id=kwargs.get("pk")
+        qs=Mobiles.objects.get(id=id)
+        serializer=MobileSerializer(qs) #deseri
+        return Response(data=serializer.data)
+    
+    def create(self,request,*args,**kwargs):
+        serializer=MobileSerializer(data=request.data) #seriali
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data)
+        else:
+            return Response(data=serializer.errors)
+        
+    def update(self,request,*args,**kwargs):
+        id=kwargs.get("pk")
+        mobile_object=Mobiles.objects.get(id=id)
+        serializer=MobileSerializer(data=request.data,instance=mobile_object) #seri
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data)
+        else:
+            return Response(data=serializer.errors)
+
+    def destroy(self,request,*args,**kwargs):
+        id=kwargs.get("pk")   
+        Mobiles.objects.get(id=id).delete()
+        return Response(data={"message":"data deleted successfully"})
     
